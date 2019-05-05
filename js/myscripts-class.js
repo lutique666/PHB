421ec2ea46fbf7a8f26b1cc2e531f40a74a68e40
lutique666 <30596655+lutique666@users.noreply.github.com>
2019-05-05 20:53:40 +0300

    Редизайн

--- js/myscripts-class.js
+++ js/myscripts-class.js
@@ -5,6 +5,7 @@ var class_main = document.getElementsByClassName('class_main');
 var current_display_archetype = document.getElementsByClassName('archetype1');
 var current_display_main = true;
 
+var table_of_contents = '<a href="Chapter00.html">Введение</a><i>Часть 1: Создание Персонажа</i><a href="Chapter01.html">Глава 1: Создание Персонажа</a><a href="Chapter02.html">Глава 2: Расы</a><a href="Chapter03.html">Глава 3: Классы</a><a href="Chapter04.html">Глава 4: Личность и Предыстория</a><a href="Chapter05.html">Глава 5: Снаряжение</a><a href="Chapter06.html">Глава 6: Индивидуальные Опции</a><a href="Chapter07.html">Глава 7: Использование Характеристик</a> <i>Часть 2: Играя в Игру</i><a href="Chapter08.html">Глава 8: Приключения</a><a href="Chapter09.html">Глава 9: Сражение</a> <i>Часть 3: Правила Магии</i><a href="Chapter10.html">Глава 10: Использование Заклинаний</a><a href="Chapter11gen.html">Глава 11: Заклинания</a><i>Приложения</i><a href="Attachment01.html">Приложение A: Состояния</a><a href="Attachment02.html">Приложение Б: Боги Мультивселенной</a><a href="Attachment03.html">Приложение В: Планы Существования</a><a href="Attachment04.html">Приложение Г: Параметры Существ</a><i>Разное</i><a href="pocket.html">Генератор краж носовых платков</a>'
 
 function openNav() {
   document.getElementById("navigation").style.width = "100%";
@@ -44,7 +45,6 @@ for (i = 0; i < archetypeCell.length-1; i++) {
       {
         archetypeCell[i].style.backgroundColor = 'orange';
         current_display_archetype= document.getElementsByClassName('archetype'+i);
-
       }
     else
     {
@@ -62,9 +62,9 @@ for (i = 0; i < current_display_archetype.length; i++) {
 function ChangeDisplayMain(button)
 {
 
-if (button == 'Показать только архетипы')
+if (button == 'Только подклассы')
 {
-  archetypeCell[archetypeCell.length-1].innerHTML='Показать полный список умений'
+  archetypeCell[archetypeCell.length-1].innerHTML='Полный список умений'
  for (i = 0; i < class_main.length; i++) {
   class_main[i].style.display='none';
   }
@@ -72,7 +72,7 @@ if (button == 'Показать только архетипы')
 
 else
 {
-  archetypeCell[archetypeCell.length-1].innerHTML='Показать только архетипы'
+  archetypeCell[archetypeCell.length-1].innerHTML='Только подклассы'
   for (i = 0; i < class_main.length; i++) {
     class_main[i].style.display='block';
   }
