ac0253c6767131267c538e9a5d192054133d7c0e
lutique666 <30596655+lutique666@users.noreply.github.com>
2019-07-28 19:51:57 +0300

    Add files via upload

--- js/creatures.js
+++ js/creatures.js
@@ -6,25 +6,30 @@ var des = document.getElementsByClassName('featherlight-inner');
 var check = 0; //Переменная для прокликивания
 var found = [];
 var spell0 = ['Зомби', 'Скелет']
-var spell1 = ['Саблезубый тигр CR2', 'Кетцалькоатль CR2', 'Полярный медведь CR2', 'Аллозавр CR2']
+var spell1 = ['Саблезубый тигр CR 2', 'Кетцалькоатль CR 2', 'Полярный медведь CR 2', 'Аллозавр CR 2']
 var spell2 = ['Гуль', 'Вурдалак', 'Умертвие', 'Мумия']
-var spell3 = ['Чвинга', 'Водный элементаль', 'Воздушный элементаль', 'Земляной элементаль', 'Огненный элементаль', 'Саламандра', 'Зорн']
+var spell3 = ['Чвинга CR 0', 'Водный элементаль CR 5', 'Воздушный элементаль CR 5', 'Земляной элементаль CR 5', 'Огненный элементаль CR 5', 'Саламандра CR 5', 'Зорн CR 5']
+var spell4 = ['Ворон CR 0', 'Кошка CR 0', 'Краб CR 0', 'Крыса CR 0', 'Летучая мышь CR 0', 'Лягушка CR 0', 'Морской конек CR 0', 'Осьминог CR 0', 'Паук CR 0', 'Рыба CR 0', 'Сова CR 0', 'Хорек CR 0', 'Ядовитая змея CR 1/8', 'Ястреб CR 0', 'Ящерица CR 0']
+var spell5 = ['Летучая мышь CR 0', 'Паук CR 0', 'Сова CR 0', 'Крыса CR 0', 'Тресум CR 0', 'Летающая змея  CR 1/8', 'Верблюд CR 1/8', 'Гигантская ездовая ящерица CR 1/4', 'Гигантский Паук-Волк CR 1/4', 'Гигантский крокодил CR 5', 'Гигантская акула CR 5', 'Мамонт CR 6', 'Гигантская обезьяна CR 7', 'Тираннозавр рекс CR 8']
 var class_name = 'spell'
+
 
 
 function pageload() {
   document.getElementsByClassName('overlay-content')[0].innerHTML = table_of_contents;
   tmp = document.URL.split('#')
-  creaturename = decodeURI(tmp[1])
-  if (creaturename == 'undefined')
-  {
-   
+  spell_tmp1 = decodeURI(tmp[1])
+  spell_tmp2 = document.getElementsByClassName('spellCell')
+  for (i = 0; i < spell_tmp2.length; i++) {
+    if (spell_tmp2[i].innerHTML.toUpperCase().indexOf(spell_tmp1.toUpperCase()) >= 0)
+    {
+     ChangeLevel(spell_tmp2[i].id) 
+    }
   }
-  else
-  {
-  Search(creaturename);
-  }
+
+
 }
+
 
 /*ПОЖАЛУЙСТА ПОФИКСИТЕ ЭТО*/
 /*Скрытие лайт бокса. */
